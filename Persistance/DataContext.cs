ce9ab8ef848a96f71339dbd55352a1afd9d4f9ff
Abdelrahman Ehab <abduehab@pop-os.localdomain>
2020-09-17 16:54:04 +0200

    finished some cuid classes

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -9,10 +9,15 @@ namespace Persistance
         {
         }
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Value>().HasData(new Value { Id = 1, Name = "Value  1" }, new Value { Id = 2, Name = "Value  2" }, new Value { Id = 3, Name = "Value  3" });
+            builder.Entity<Value>()
+            .HasData(new Value { Id = 1, Name = "Value  1" },
+                     new Value { Id = 2, Name = "Value  2" },
+                     new Value { Id = 3, Name = "Value  3" }
+                     );
         }
     }
 }
---
+++ Persistance/Migrations/20200828202910_NewActivityEntityAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class NewActivityEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Activities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Activities");
+        }
+    }
+}
